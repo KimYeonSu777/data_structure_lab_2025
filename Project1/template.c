da15f356ccbcc3fdf7ec212cf97fdd1dd7d3a4c1
CheckmateFY <kbsbsx@naver.com>
2025-03-19 08:54:43 +0900

    clear(1)

---
+++ Project1/template.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+int main(void) {
+	int test_case;
+	int T;
+
+	freopen("input.txt", "r", stdin);
+	setbuf(stdout, NULL);
+	scanf("%d", &T);
+
+	for (test_case = 1; test_case <= T; ++test_case) {
+
+	}
+	return 0;
+}
